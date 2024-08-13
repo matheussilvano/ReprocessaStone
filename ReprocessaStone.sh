3f036d24af975eed307d737b72f04255aadc7383
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-08-13 16:52:25 -0300

    Create ReprocessaStone.sh

---
+++ ReprocessaStone.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+read -p "Insira um nome para o mapa (Ex: SUPERMERCADO_IDEAL): " NomeMapa
+read -p "Insira o CNPJ do cliente sem pontos e traços: " CnpjCliente
+read -p "Insira o Stone Code do cliente: " StoneCode
+read -p "Insira o id da Caixa Postal do cliente: " IdCaixaPostal
+read -p "Insira o número de dias retroativos: " DiasRetroativos
+clear
+cd /home/skyline/operacoes/DEMO.SILVANO/ReprocessamentosStone
+
+
+
+
+
