4b230284bc9c74c0aebd0fcdb823f26db7440f55
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-08-13 20:57:53 -0300

    Update ReprocessaStone.sh

--- ReprocessaStone.sh
+++ ReprocessaStone.sh
@@ -6,14 +6,21 @@ read -p "Insira o CNPJ do cliente sem pontos e traços: " CnpjCliente
 read -p "Insira o Stone Code do cliente: " StoneCode
 read -p "Insira o id da Caixa Postal do cliente: " IdCaixaPostal
 read -p "Insira o número de dias retroativos: " DiasRetroativos
-clear
 
 # Variaveis
 ConteudoMapa = "[$NomeMapa]
 authorization0=$CnpjCliente;$StoneCode;RET.EXT.$StoneCode.390.$IdCaixaPostal"
 Arquivo = $NomeMapa.ini
+Demo = home/skyline/operacoes/DEMO.SILVANO/ReprocessamentosStone
 
-$ConteudoMapa > /home/skyline/operacoes/DEMO.SILVANO/ReprocessamentosStone/$Arquivo
+touch $Demo/$Arquivo
+$ConteudoMapa >> $Demo/$Arquivo
+
+/home/skyline/scripts/stone/StoneGetFiles.sh.run $Demo/$Arquivo -r $DiasRetroativos
+
+cd /home/skyline/scripts/stone/python/reprocessamento
+
+for i in *RET.EXT.$StoneCode.390.$IdCaixaPostal* ; do mv -v $i /var/spool/nexxera/skyline/recebe/ident/$(basename $i | cut -f1 -d\$) ; sleep 1 ; done
 
 
 
