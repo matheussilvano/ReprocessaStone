ae20268139406058d2335a2a3a0908e32ba9a6f4
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-08-13 17:01:52 -0300

    Update ReprocessaStone.sh

--- ReprocessaStone.sh
+++ ReprocessaStone.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 
+# Inserir dados do cliente
 read -p "Insira um nome para o mapa (Ex: SUPERMERCADO_IDEAL): " NomeMapa
 read -p "Insira o CNPJ do cliente sem pontos e traços: " CnpjCliente
 read -p "Insira o Stone Code do cliente: " StoneCode
 read -p "Insira o id da Caixa Postal do cliente: " IdCaixaPostal
 read -p "Insira o número de dias retroativos: " DiasRetroativos
 clear
+
+# Variaveis
+ConteudoMapa = "[$NomeMapa]
+authorization0=$CnpjCliente;$StoneCode;RET.EXT.$StoneCode.390.$IdCaixaPostal"
+Arquivo = $NomeMapa.ini
+
 cd /home/skyline/operacoes/DEMO.SILVANO/ReprocessamentosStone
+touch $Arquivo
+$ConteudoMapa > $Arquivo
+
+
 
 
 
